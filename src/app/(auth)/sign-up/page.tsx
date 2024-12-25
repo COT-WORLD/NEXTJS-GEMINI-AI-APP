6a023d655cbcc1fd73bfd9ddb53e4162f8282b68
COT-WORLD <hardikchaudhary.aus@gmail.com>
2024-12-25 15:50:46 +1100

    complete frontend with shadcn library, tailwind, axois, Google Gemini API

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,170 @@
+'use client'
+import {z } from "zod"
+import {zodResolver} from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { useDebounceValue } from 'usehooks-ts'
+import { useToast } from "@/hooks/use-toast"
+import { useRouter } from "next/navigation"
+import { signUpSchema } from "@/schemas/signUpSchema"
+import axios, {AxiosError} from 'axios'
+import { ApiResponse } from "@/types/ApiResponse"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react"
+
+
+const page= () => {
+
+  const [username, setUsername] = useState("")
+  const [usernameMessage, setUsernameMessage] = useState("")
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  // const debouncedUsername= useDebouncevalue(username, 300)
+  const [debouncedUsername, setDebouncedUsername] = useDebounceValue(username, 500)
+
+  const { toast } = useToast()
+
+  const router = useRouter()
+
+  //zod implementation
+
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver:zodResolver(signUpSchema),
+    defaultValues:{
+      username:"",
+      email:"",
+      password:"",
+    }
+  })
+
+  useEffect(()=>{
+    const checkUsernameUnique = async () => {
+      if (debouncedUsername) {
+        setIsCheckingUsername(true)
+        setUsernameMessage('')
+        try {
+          const response=await axios.get(`/api/check-username-unique?username=${debouncedUsername}`)
+          setUsernameMessage(response.data.message)
+        } catch (error) {
+          const axiosError = error as AxiosError<ApiResponse>
+          setUsernameMessage(axiosError.response?.data.message?? "Error checking username")
+        }finally{
+          setIsCheckingUsername(false)
+        }
+      }
+    }
+    checkUsernameUnique()
+  },[debouncedUsername])
+
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true)
+    try {
+      const response = await axios.post<ApiResponse>('/api/sign-up', data)
+      if (response.data.success) {
+        toast({
+          title: 'Success',
+          description: response.data.message
+        })
+        router.replace(`/verify-code/${username}`)
+      }
+    } catch (error) {
+      console.error("Error in signup of user", error)
+      const axiosError = error as AxiosError<ApiResponse>
+      let errorMessage = axiosError.response?.data.message
+      toast({
+        title: "Signup failed",
+        description: errorMessage,
+        variant: "destructive"
+      })
+    } finally{
+      setIsSubmitting(false)
+    }
+  }
+
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">Join Unforseen Message</h1>
+          <p className="mb-4">Sign up to start your Unforseen advanture</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="username"
+              control={form.control}
+              render={({ field })=>(
+                <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="username" {...field}
+                  onChange={(e)=>{
+                    field.onChange(e)
+                    setUsername(e.target.value)
+                  }}/>
+                  </FormControl>
+                  {isCheckingUsername && <Loader2 className="animate-spin" />}
+                  <p className={`text-sm ${usernameMessage === "Username is unique and available"? 'text-green-500': 'text-red-500' }`}>
+                    {usernameMessage}
+                  </p>
+                  <FormMessage/>
+              </FormItem>
+
+              //TODo: video time 5:59:17
+              )}
+            />
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field })=>(
+                <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="email" {...field}/>
+                  </FormControl>
+                  <FormMessage/>
+              </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field })=>(
+                <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="password" {...field}/>
+                  </FormControl>
+                  <FormMessage/>
+              </FormItem>
+              )}
+            />
+            
+            <Button type="submit" disabled={isSubmitting}>{
+              isSubmitting?
+              <>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait 
+              </>
+              : ("Signup")
+              }</Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Already a member?{ ' '}
+            <Link href={"/sign-in"} className="text-blue-600 hover:text-blue-800">
+            Sign in
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/u/[username]/page.tsx
@@ -0,0 +1,114 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Textarea } from '@/components/ui/textarea'
+import { useToast } from '@/hooks/use-toast'
+import { messageSchema } from '@/schemas/messageSchema'
+import { ApiResponse } from '@/types/ApiResponse'
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios, { AxiosError } from 'axios'
+import { useParams, useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import * as z  from 'zod'
+
+function Message() {
+  const [suggestedMessages, setSuggestedMessages] = useState([])
+  const params = useParams<{username:string}>()
+  const {toast} = useToast()
+  const form = useForm<z.infer<typeof messageSchema>>({
+    resolver:zodResolver(messageSchema),
+    defaultValues:{
+      content:""
+    }
+  })
+
+  const onSubmit = async (data:z.infer<typeof messageSchema>) => {
+    try {
+      const response= await axios.post(`/api/send-message`,{
+        username: params.username,
+        content: data.content
+      })
+      if (response.data.success) {
+        toast({
+          title: "Message sent succesfully",
+          variant:"default"
+        })
+        form.reset()
+      }
+      
+    } catch (error) {
+      console.error("Error in sending message to user", error)
+      const axiosError = error as AxiosError<ApiResponse>
+      let errorMessage = axiosError.response?.data.message
+      toast({
+        title: "Message send to user failed",
+        description: errorMessage,
+        variant: "destructive"
+      })
+    }
+  }
+
+  const fecthAIMessage = async () =>{
+    try {
+      const response = await axios.get(`/api/suggest-messages`)
+      if (response.data.success) {
+        setSuggestedMessages(response.data.messages)
+      }
+    } catch (error) {
+      console.error("Error while retriving messages from AI", error)
+      const axiosError = error as AxiosError<ApiResponse>
+      let errorMessage = axiosError.response?.data.message
+      toast({
+        title: "Retriving messages from AI failed",
+        description: errorMessage,
+        variant: "destructive"
+      })
+    }
+  }
+  return (
+    <>    
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">Public Profile Link</h1>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="content"
+              control={form.control}
+              render={({ field })=>(
+                <FormItem>
+                <FormLabel>Send Annonymous Message to @{params.username}</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="Write your annonymous message here" {...field}/>
+                  </FormControl>
+                  <FormMessage/>
+              </FormItem>
+              )}
+            />            
+            <Button type="submit">Send It</Button>
+          </form>
+        </Form>
+          <Button onClick={fecthAIMessage}>Suggest Messages</Button>
+          <h1>Click on any message below to select it.</h1>
+          <div className='w-full p-2 space-y-8 bg-white rounded-lg shadow-md'>
+            <h1 className="text-xl font-extrabold tracking-tight lg:text-5xl mb-6">Messages</h1>
+            {
+            suggestedMessages.length > 0 ? (
+            suggestedMessages.map((message, index)=>(
+              <Button key={index} variant="secondary" onClick={()=>(form.setValue('content', message, { shouldValidate: true }))}>{message}</Button>
+            ))
+          ):(
+            <p>No messages to display</p>
+          )
+        }
+          </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Message
